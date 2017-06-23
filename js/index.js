7f8470e4f8b8721c99edaa6361c6f24e3eca9e00
Lains <lainsce@airmail.cc>
2017-06-22 22:03:31 -0300

    Scrap the auto updating age thing

--- js/index.js
+++
@@ -1,7 +0,0 @@
-var birth = new Date('06/06/1993');
-var check = new Date();
-var milliDay = 1000 * 60 * 60 * 24; // a day in milliseconds;
-var ageInDays = (check - birth) / milliDay;
-var ageInYears =  Math.floor(ageInDays / 365 );
-
-document.getElementById('age').innerText= ageInYears;
