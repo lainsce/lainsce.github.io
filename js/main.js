cbb874bf7b3b5bb47afe706f8b3c2d9960e0e3e0
Lains <lainsce@airmail.cc>
2017-06-22 23:19:37 -0300

    Try a new way of calculating the age

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 var birth = new Date('1993-06-06');
-var now = new Date();
-var difference = now - birth;
-// Dates are set from 1970, so remove that to get the year
-var age = new Date(difference).getFullYear() - 1970
+function getAge(birth) {
+  ageMS = Date.parse(Date()) - Date.parse(birth);
+  age = new Date();
+  age.setTime(ageMS);
+  ageYear = age.getFullYear() - 1970;
 
-var span = document.getElementById('age');
-span.innerHTML = age;
+  return ageYear;
+}
+
+document.getElementById('age').innerHTML = getAge(birth);
