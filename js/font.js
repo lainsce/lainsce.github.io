4c477e73719d9ea4ed74bbfc68cc09ea97a244d3
Lains <lainsce@airmail.cc>
2017-06-30 21:17:00 -0300

    Make fa-icons async too

--- js/font.js
+++ js/font.js
@@ -1,5 +1,8 @@
 $(function() {
   var fonts = 'https://fonts.googleapis.com/css?family=Raleway:300|Roboto:300,400';
+  var icons = 'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css';
   if (document.createStyleSheet) document.createStyleSheet(fonts);
   else $("head").append($("<link rel='stylesheet' href='"+ fonts +"' type='text/css' media='screen' />"));
+  if (document.createStyleSheet) document.createStyleSheet(icons);
+  else $("head").append($("<link rel='stylesheet' href='"+ icons +"' type='text/css' media='screen' />"));
 });
