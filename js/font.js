5c926882b2ac422322d6c12a172bf47c6a0e64ae
Lains <lainsce@airmail.cc>
2017-06-30 23:23:13 -0300

    inline this js

--- js/font.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  var fonts = 'https://fonts.googleapis.com/css?family=Raleway:300|Roboto:300,400,500';
-  if (document.createStyleSheet) document.createStyleSheet(fonts);
-  else $("head").append($("<link rel='stylesheet' href='"+ fonts +"' type='text/css' media='screen' />"));
-});
