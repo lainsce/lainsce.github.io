a272c2665daad46e8febd497644d91561dd3dc6f
Paulo Galardi <lainsce@airmail.cc>
2017-09-26 19:31:28 -0300

    recalc the navbar scroll.js math

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(window).on("scroll", function () {
       var wn = $(window).scrollTop();
-      if (wn > 580) {
+      if (wn > 546) {
         $(".nav").css("background", "#4D4D4D");
       } else {
         $(".nav").css("background", "rgba(0,0,0,0)");
