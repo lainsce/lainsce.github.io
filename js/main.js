cd6b420a2911fb44a07a582750b467052b14e76b
Lains <lains@airmail.cc>
2021-01-25 21:22:00 -0300

    * - remove nataniev stuff

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
-function arvelie (date = new Date()) {
-    const start = new Date(date.getFullYear(), 0, 0);
-    const diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
-    const doty = Math.floor(diff / 86400000) - 1;
-    const l = Math.floor(((doty) / 364) * 26);
-    const y = date.getFullYear().toString().substr(2, 2)
-    const m = doty === 364 || doty === 365 ? '+' : String.fromCharCode(97 + l).toUpperCase();
-    const d = `${(doty === 365 ? 1 : doty === 366 ? 2 : (doty % 14)) + 1}`.padStart(2, '0');
-    return `${y}${m}${d}`;
-}
-
 today = new Date();
-document.getElementById('year').innerHTML = arvelie(today);
+document.getElementById('year').innerHTML = today.getFullYear();
 
 var birth = new Date('1993-06-06');
 function getAge(birth) {
