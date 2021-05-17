a64ad5638be2c464e582f80e4e605f66a24d4e23
Lains <lainsce@airmail.cc>
2021-05-17 17:45:28 -0300

    *

--- js/main.js
+++ js/main.js
@@ -1,12 +1,2 @@
 today = new Date();
 document.getElementById('year').innerHTML = today.getFullYear();
-
-var birth = new Date('1993-06-06');
-function getAge(birth) {
-  ageMS = Date.parse(Date()) - Date.parse(birth);
-  age = new Date();
-  age.setTime(ageMS);
-  ageYear = age.getFullYear() - 1970;
-  return ageYear;
-}
-document.getElementById('age').innerHTML = getAge(birth);
--- js/theme.js
+++ js/theme.js
@@ -47,3 +47,94 @@ const toggleBtn = document.getElementById('theme-toggle');
 toggleBtn.addEventListener('click', toggleTheme);
 
 setPreferredTheme();
+
+let mcursor = document.querySelector('.cursor');
+window.addEventListener('mousemove', mouse);
+
+let textCursorNodes = Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, td, th, dt, q'));
+textCursorNodes.forEach((el)=> bindTextCursorNodes(el));
+
+let btnNodes = Array.from(document.querySelectorAll('.button, #theme-toggle'));
+btnNodes.forEach((el)=> this.bindBtnNodes(el));
+
+let isCursorLocked = false;
+let isTouchLocked = false;
+const DEFAULT_CURSOR_SIZE = 36;
+
+function mouse(e){
+  if (!this.isCursorLocked && !this.isTouchLocked) {
+    mcursor.style.top = e.pageY + 'px';
+    mcursor.style.left = e.pageX + 'px';
+  }
+}
+
+function bindTextCursorNodes(e){
+  let fontSize = window.getComputedStyle(e).getPropertyValue('font-size').replace('px', '');
+  e.addEventListener(
+      'mouseover',
+      () => {
+          mcursor.style.height = fontSize * 1.4 + 'px';
+          mcursor.classList.add('cursor-text');
+      }
+  );
+  e.addEventListener(
+      'mouseout',
+      () => {
+          mcursor.removeAttribute('style');
+          mcursor.classList.remove('cursor-text');
+      }
+  );
+}
+
+function bindBtnNodes(el){
+    let rect = null;
+
+    el.addEventListener('mouseenter', (event)=>{
+        if (isTouchLocked) return;
+
+        isCursorLocked = true;
+
+        rect = el.getBoundingClientRect();
+
+
+        let borderRadius = window.getComputedStyle(el).getPropertyValue('border-radius').replace('px', '');
+
+        mcursor.classList.add('is-locked');
+        mcursor.style.width = rect.width + 'px';
+        mcursor.style.height = rect.height + 'px';
+        mcursor.style.borderRadius = borderRadius + 'px';
+        mcursor.style.left = rect.x + window.pageXOffset + rect.width/2 + 'px';
+        mcursor.style.top = rect.y + window.pageYOffset + rect.height/2 + 'px';
+    });
+
+    el.addEventListener('mousemove', (event)=>{
+        if (isTouchLocked) return;
+
+        const halfHeight = rect.height / 2;
+        const topOffset = (event.y - rect.top - halfHeight) / halfHeight;
+        const halfWidth = rect.width / 2;
+        const leftOffset = (event.x - rect.left - halfWidth) / halfWidth;
+
+        mcursor.style.transform = `translate(calc(-50% + ${leftOffset}px), calc(-50% + ${topOffset}px))`;
+
+        if (el.matches('.panel, a:not(.fab)')){
+            el.style.transform = `translate(${leftOffset*6}px, ${topOffset*6}px)`;
+        }
+    });
+
+    el.addEventListener('mouseleave', (event)=>{
+        if (isTouchLocked) return;
+
+        isCursorLocked = false;
+        mcursor.classList.remove('is-locked');
+        mcursor.classList.remove('cursor--text');
+        mcursor.style.borderRadius = '100%';
+        mcursor.style.width = DEFAULT_CURSOR_SIZE + 'px';
+        mcursor.style.height = DEFAULT_CURSOR_SIZE + 'px';
+
+        el.removeAttribute('style');
+        if (el.firstElementChild){
+            el.firstElementChild.removeAttribute('style');
+        }
+    });
+}
